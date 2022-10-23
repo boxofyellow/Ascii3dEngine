56383308c8a0046b8ebeefa1f6850fc0d16b1898
boxofyellow <boxofyellow@github.com>
2022-10-23 17:25:37 -0400

    We can't use Transform in that way 🙃

--- Engine/Support/Point3D.cs
+++ Engine/Support/Point3D.cs
@@ -118,8 +118,7 @@ public readonly struct Point3D
     public override int GetHashCode()
         => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
 
-    // We should do some perf testing, should we we use something like this * and /?
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    // This should likely only get used for tests, we find production uses we should probably do some performance testing
     public Point3D Transform(Func<double, double> transformation)
         => new (transformation(X), transformation(Y), transformation(Z));
 
